d9a547a4a20e4896fe2d465f9d6e05622a01f363
ishi-0409 <isaku0409@icloud.com>
2025-05-29 01:46:43 +0900

    test.pyを削除

--- RGB.py
+++ RGB.py
@@ -7,6 +7,12 @@ RED=23
 GREEN=24
 BLUE=21
 
+GREENbutton=6
+YELLOWbutton=5
+
+GPIO.setup(GREENbutton,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(YELLOWbutton,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
 GPIO.setup(RED,GPIO.OUT)
 GPIO.setup(GREEN,GPIO.OUT)
 GPIO.setup(BLUE,GPIO.OUT)
@@ -20,6 +26,7 @@ GREENpwm.start(0)
 BLUEpwm.start(0)
 
 ADC0834.setup()
+index=[0,0,0]
 
 try:
     while True:
@@ -33,10 +40,21 @@ try:
         REDpwm.ChangeDutyCycle(REDDC)
         GREENpwm.ChangeDutyCycle(GREENDC)
         BLUEpwm.ChangeDutyCycle(BLUEDC)
-        if REDDC<=5 or GREENDC<=5 or BLUEDC<=5:
-            REDDC==0,GREENDC==0,BLUEDC==0
+        if GPIO.input(GREENbutton)==GPIO.LOW:
+            index=[REDDC,GREENDC,BLUEDC]
+            print('save the color',index)
+            sleep(0.3)
+        if GPIO.input(YELLOWbutton)==GPIO.LOW:
+            REDpwm.ChangeDutyCycle(index[0])
+            GREENpwm.ChangeDutyCycle(index[1])
+            BLUEpwm.ChangeDutyCycle(index[2])
+            print('Update')
+            sleep(0.3)
         sleep(0.3)
 except KeyboardInterrupt:
+    REDpwm.stop()
+    GREENpwm.stop()
+    BLUEpwm.stop()
     GPIO.cleanup()
 
 
